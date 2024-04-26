02992a8de3a5fac43cf980d720c7fc22c03e28c6
菜菜 <3104591385@qq.com>
2024-04-26 08:04:44 +0800

    0.03

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,8 @@ const setGlobalData = (obj: any) => {
 
 <template>
   <Sign 
+  source="http://shipin.cdxyhpx.com/sv/3bba3a21-18926843f74/3bba3a21-18926843f74.mp4"
+    vid="a175d6b01b4271ee80980764b3ec0102" playauth="" 
       :global-data="globalData"
     :set-global-data="setGlobalData" :course_log_pk="0" />
 </template>
