6ec7d1aa187267358bfb882ccf336461ec08ba72
菜菜 <3104591385@qq.com>
2024-04-26 08:44:23 +0800

    0.1.3

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import Sign from "./components/Sign.vue";
 import { reactive, ref } from "vue";
 
-const globalData = reactive({ componentName: "移动端手写签名组件-zion" });
+const globalData = reactive({ value: "http://shipin.cdxyhpx.com/sv/3bba3a21-18926843f74/3bba3a21-18926843f74.mp4",componentName: "移动端手写签名组件-zion" });
 const setGlobalData = (obj: any) => {
   globalData.value = obj.value;
 };
@@ -11,11 +11,13 @@ const setGlobalData = (obj: any) => {
 // const actionflow_id = ref("5e5e155d-7b3c-4771-bf94-ceb597cbb666")
 
 const url = ref("");
-const actionflow_id = ref("");
+const actionflow_id = ref("39b8c22f-5d78-446b-a046-a2967986da5c")
+
 </script>
 
 <template>
   <Sign
+  source="http://shipin.cdxyhpx.com/sv/3bba3a21-18926843f74/3bba3a21-18926843f74.mp4"
     vid="a175d6b01b4271ee80980764b3ec0102"
     playauth=""
     :url="url"
