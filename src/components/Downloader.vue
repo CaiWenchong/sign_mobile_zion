9193aa0d91457a0848654cddd13a25899478ad47
菜菜 <3104591385@qq.com>
2024-04-26 08:22:26 +0800

    0.0.7

--- src/components/Downloader.vue
+++ src/components/Downloader.vue
@@ -25,7 +25,7 @@
 import CryptoJS from "crypto-js";
 export default {
   name: "Downloader",
-  props: ["globalData", "url", "actionflow_id", "signId"],
+  props: ["globalData", "url", "actionflow_id"],
   data() {
     return {
       mdapi: null,
